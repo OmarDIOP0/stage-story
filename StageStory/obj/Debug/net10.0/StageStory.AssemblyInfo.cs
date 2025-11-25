3fcbee0f93462ae9075f651a67f08454077b49b1
OmarDIOP0 <odiop6170@gmail.com>
2025-11-25 00:36:16 +0000

    installation de tailwind

--- StageStory/obj/Debug/net10.0/StageStory.AssemblyInfo.cs
+++ StageStory/obj/Debug/net10.0/StageStory.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StageStory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a68e2ea98137912464aec396b96dc5e4aa01551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f2945aa387b56364d998962ded24bef8f2a0e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StageStory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StageStory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
