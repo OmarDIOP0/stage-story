79a6b41175f3009048e72ef7d3a88eab84845bf0
OmarDIOP0 <odiop6170@gmail.com>
2025-11-27 23:14:55 +0000

    ajout de la classe intership

--- StageStory/Models/Student.cs
+++ StageStory/Models/Student.cs
@@ -7,13 +7,15 @@ namespace StageStory.Models
         []
         public int StudentId { get; set; }
         public string Name { get; set; }
+        public string? Gender { get; set; }
         public ProfileEnum Profile { get; set; }
-        public string Email { get; set; }
-        public string Level { get; set; }
-        public string Specialization { get; set; }
+        public string? Email { get; set; }
+        public string? Level { get; set; }
+        public string? Specialization { get; set; }
         public int Year { get; set; }
-        public string University { get; set; }
+        public string? University { get; set; }
 
         public virtual ICollection<Notification> Notifications { get; set; }
+        public virtual ICollection<Internship> Internships { get; set; }
     }
 }
