a286db73769b3dfaa15d18fd458cc00f0d359262
OmarDIOP0 <odiop6170@gmail.com>
2025-11-28 20:18:13 +0000

    implementation de la page index

---
+++ StageStory/Helper/JwtHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace StageStory.Helper
+{
+    public class JwtHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(Claim[] claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "StageStory",
+                audience: "StageStory",
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ StageStory/Models/Dto/AdminLoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace StageStory.Models.Dto
+{
+    public class AdminLoginResponse
+    {
+        public string? Token { get; set; }
+        public DateTime TokenExpireAt { get; set; }
+        public string? RefreshToken { get; set; }
+        public Admin? Admin { get; set; }
+    }
+}
