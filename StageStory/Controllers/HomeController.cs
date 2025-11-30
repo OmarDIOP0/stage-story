48aecf797202d699d05518ebe6545b8155908c75
OmarDIOP0 <odiop6170@gmail.com>
2025-11-30 19:37:59 +0000

    ajout de la methode de creation de stage dans Home

--- StageStory/Controllers/HomeController.cs
+++ StageStory/Controllers/HomeController.cs
@@ -20,6 +20,36 @@ public class HomeController : Controller
         ViewBag.EntrepriseList = new SelectList(_context.Enterprises, "Id", "Name");
         return View();
     }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Description,Rating,Evaluation,SalaryAmount,Status,SignatureType,StartDate,EndDate,CreatedDate,StudentId,EntrepriseId")] Internship internship)
+    {
+        if (!ModelState.IsValid)
+        {
+            var allErrors = ModelState.Values.SelectMany(v => v.Errors)
+                                             .Select(e => e.ErrorMessage)
+                                             .ToList();
+            return Json(new { success = false, errors = allErrors });
+        }
+
+        internship.CreatedDate = DateTime.Now;
+        internship.Status = Models.Enum.StatusEnum.Pending;
+
+        if (internship.StudentId == 0)
+        {
+            internship.Profile = Models.Enum.ProfileEnum.Anonymous;
+            internship.StudentId = null;
+        }
+        else
+        {
+            internship.Profile = Models.Enum.ProfileEnum.Student;
+        }
+
+        _context.Add(internship);
+        await _context.SaveChangesAsync();
+
+        return Json(new { success = true, message = "Stage créé avec succès !" });
+    }
 
     public IActionResult Privacy()
     {
