09440d4418f9043171af5c248d654ce826c5693c
OmarDIOP0 <odiop6170@gmail.com>
2025-11-30 16:41:42 +0000

    modification du formulaire

--- StageStory/Controllers/HomeController.cs
+++ StageStory/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using StageStory.Data;
 using StageStory.Models;
+using System.Diagnostics;
 
 namespace StageStory.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }   
     public IActionResult Index()
     {
+        ViewBag.EntrepriseList = new SelectList(_context.Enterprises, "Id", "Name");
         return View();
     }
 
